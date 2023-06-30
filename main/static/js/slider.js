d204b68bd9340cfae9ec787a90cdaa62a63f2e19
l1can13 <a.a.soroka05@gmail.com>
2023-06-30 18:17:11 +0400

    slider done, master's works grid done, masters started

--- main/static/js/slider.js
+++ main/static/js/slider.js
@@ -24,4 +24,21 @@ $(document).ready(function () {
             }
         ]
     });
+
+    const sliderItems = document.querySelectorAll('.slider__item div');
+    const insideBoxH2s = document.querySelectorAll('.inside_box h2');
+
+    sliderItems.forEach((sliderItem, index) => {
+        const insideBoxH2 = insideBoxH2s[index];
+
+        sliderItem.addEventListener('mouseenter', () => {
+            sliderItem.style.background = 'rgba(0, 0, 0, 0.74)';
+            insideBoxH2.style.opacity = '1';
+        });
+
+        sliderItem.addEventListener('mouseleave', () => {
+            sliderItem.style.background = '';
+            insideBoxH2.style.opacity = '';
+        });
+    });
 });
