5292e6c7d888092c840f6f9775dd7ff842c9b547
l1can13 <a.a.soroka05@gmail.com>
2023-09-01 13:55:47 +0400

    frontend dynamic team done

--- main/static/js/index.js
+++ main/static/js/index.js
@@ -9,3 +9,48 @@ bookSessionButton.addEventListener('click', (event) => {
         scrollToElement(aboutContainer);
     }
 });
+
+$(document).ready(function () {
+    function updateGrid() {
+        var mastersContainer = $(".masters_first");
+        var masters = mastersContainer.find(".master");
+
+        var screenWidth = $(window).width();
+        var maxColumns = Math.floor(screenWidth / 500); // Assuming 300px width for each item
+        var maxItemsPerRow = Math.min(maxColumns, 3); // Максимум 3 элемента в строке
+
+        // Группировка мастеров по группам с максимум 3 элементами
+        var masterGroups = [];
+        for (var i = 0; i < masters.length; i += maxItemsPerRow) {
+            var group = masters.slice(i, i + maxItemsPerRow);
+            masterGroups.push(group);
+        }
+
+        // Удалить все существующие сетки
+        mastersContainer.empty();
+
+        // Создать сетку для каждой группы мастеров
+        for (var i = 0; i < masterGroups.length; i++) {
+            var group = masterGroups[i];
+            var groupContainer = $("<div class='group'></div>");
+
+            // Если количество элементов в группе меньше 3, то добавить класс centered
+            if (group.length < maxItemsPerRow) {
+                groupContainer.addClass("centered");
+            }
+
+            groupContainer.append(group);
+            groupContainer.appendTo(mastersContainer);
+        }
+    }
+
+    // Исключите функцию updateGrid() из события resize, чтобы избежать многократных вызовов
+    var resizeTimeout;
+    $(window).resize(function () {
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(updateGrid, 1);
+    });
+
+    // Инициализируйте grid при загрузке страницы
+    updateGrid();
+});
--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from master.models import Master
+
 
 def index(request):
     context = {
         'logo_image': 'source/common/logo.png',
+        'masters': Master.objects.all()
     }
 
     return render(request, 'main/html/index.html', context)
